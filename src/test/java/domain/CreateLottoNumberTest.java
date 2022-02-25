4b1df965027dffe0f97f2cb8e54ac3e7a3c99c7f
Egenieee <hhgg0925@naver.com>
2022-02-25 15:58:05 +0900

    feat: CreateLottoNumberTest 작성

--- src/test/java/domain/CreateLottoNumberTest.java
+++ src/test/java/domain/CreateLottoNumberTest.java
@@ -1,4 +1,28 @@
 package domain;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CreateLottoNumberTest {
+    static private final int LOTTO_NUM_PER_ONE_LINE = 6;
+    static private final int LOTTO_NUM_START_INCLUSIVE = 1;
+    static private final int LOTTO_NUM_END_INCLUSIVE = 45;
+
+    @Test
+    public void 랜덤한_숫자_생성() {
+        int lottoNumber = CreateLottoNumber.getRandomNumber(LOTTO_NUM_START_INCLUSIVE, LOTTO_NUM_END_INCLUSIVE);
+
+        assertThat(lottoNumber).isBetween(LOTTO_NUM_START_INCLUSIVE, LOTTO_NUM_END_INCLUSIVE);
+    }
+
+    @Test
+    public void 로또_생성() {
+        List<Integer> lottoes = CreateLottoNumber.createLottosNumber();
+
+        assertThat(lottoes.size()).isEqualTo(LOTTO_NUM_PER_ONE_LINE);
+    }
+
 }
