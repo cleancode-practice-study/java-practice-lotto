207e5d3e0afbff87539e43cd0f0f62a80c13c109
Egenieee <hhgg0925@naver.com>
2022-02-28 18:04:36 +0900

    -refactor: Application 변수명 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,7 @@ import controller.Controller;
 
 public class Application {
 	public static void main(String[] args) {
-		Controller gameHelper = new Controller();
-		gameHelper.run();
+		Controller controller = new Controller();
+		controller.run();
 	}
 }
