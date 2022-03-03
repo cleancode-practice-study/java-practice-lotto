089ae9412a95d2eb16521a315e1bfb9b7b09dde1
Egenieee <hhgg0925@naver.com>
2022-03-03 20:35:11 +0900

    -refactor: 불필요한 주석 삭제 처리

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -16,58 +16,44 @@ public class Controller {
         printResult(winningLotto, lottoes);
     }
 
-    // 랜덤 로또 받아서 출력
     private Lottoes getLottoes() {
-        // 얼마 어치의 로또를 구매할 건지 입력
         int lottoPrice = InputView.inputLottoPurchasePrice();
         int lottoCount = GameHelper.getLottoCount(lottoPrice);
 
-        // 몇 개의 로또를 구매했는 지 출력
         OutputView.printLottoCount(lottoCount);
 
         Lottoes lottoes = GameHelper.createLottoes(lottoCount);
 
-        // 구매한 로또 번호 출력
         OutputView.printLottoNumber(lottoes);
-
         OutputView.printNextLine();
 
         return lottoes;
     }
 
-    // 당첨 로또 받기
     private WinningLotto getWinningLotto() {
         String winningNumbers = null;
         boolean isValid = false;
 
-        // 당첨 번호 입력을 중복으로 했을 경우 다시 입력 받도록 함.
         while (!isValid) {
-            // 당첨 로또 번호 받기
             winningNumbers = InputView.inputWinningLottoNumber();
-            // 중복인지 확인
+
             isValid = GameHelper.checkDuplicatedNumber(winningNumbers);
 
-            // 중복이면 오류 메시지 출력 후 다시 받도록 함.
             if (!isValid) {
                 OutputView.printInvalidLottoNumberError();
             }
         }
 
-        // 당첨 로또 보너스 받기
         int bonusNum = InputView.inputWinningBonusNumber();
 
         return GameHelper.createWinningLotto(winningNumbers, bonusNum);
     }
 
-    // 모델에게 값 받아 로또 당첨 결과 출력
     private void printResult(WinningLotto winningLotto, Lottoes lottoes) {
-        // 당첨 결과 구하기
         Map<Rank, Integer> lottoWinResult = GameHelper.getLottoWinResult(winningLotto, lottoes);
 
-        // 수익률 구하기
         double yield = GameHelper.getYield(lottoWinResult, lottoes.getLottoesCount() * LOTTO_PRICE_PER_ONE);
 
-        // 당첨 통계 출력
         OutputView.printLottoResult(lottoWinResult);
         OutputView.printLottoYield(yield);
     }
--- src/main/java/domain/GameHelper.java
+++ src/main/java/domain/GameHelper.java
@@ -6,12 +6,10 @@ public class GameHelper {
     private static final int LOTTO_PRICE_PER_ONE = 1000;
     private static final int LOTTO_NUM_PER_ONE_LINE = 6;
 
-    // 로또 금액 받아서 몇개 산 건지 계산
     public static int getLottoCount(int lottoPrice) {
         return lottoPrice / LOTTO_PRICE_PER_ONE;
     }
 
-    // 로또 생성
     public static Lottoes createLottoes(int lottoCount) {
         List<Lotto> lottoes = new ArrayList<>();
 
@@ -23,13 +21,11 @@ public class GameHelper {
         return new Lottoes(lottoes);
     }
 
-    // 랜덤 숫자로 이루어진 하나의 로또 객체 생성하기.
     private static Lotto createOneLotto() {
         List<Integer> lottoNums = LottoNumberCreator.createLottosNumber();
         return new Lotto(lottoNums);
     }
 
-    // 당첨 로또 생성
     public static WinningLotto createWinningLotto(String winningNumbers, int bonusNumber) {
         Set<Integer> splitNumbers = splitWinnigLottoNumber(winningNumbers);
         List<Integer> winningLottoNumber = new ArrayList<>(splitNumbers);
@@ -44,7 +40,6 @@ public class GameHelper {
         return splitWinningNumbers.size() == LOTTO_NUM_PER_ONE_LINE;
     }
 
-    // 입력한 지난번 당첨 로또 번호 split -> List에 담아서 리턴
     private static Set<Integer> splitWinnigLottoNumber(String winningNumbers) {
         String[] splitNumbers = winningNumbers.split(",");
         Set<Integer> winningLottoNumber = new HashSet<>();
@@ -57,7 +52,6 @@ public class GameHelper {
     }
 
     public static Map<Rank, Integer> getLottoWinResult(WinningLotto winningLotto, Lottoes lottoes) {
-        // 맞은 로또 결과 저장할 Map 초기화
         Map<Rank, Integer> lottoResult = createResult();
 
         lottoResult = lottoes.getLottoWinResult(lottoResult, winningLotto);
@@ -65,7 +59,6 @@ public class GameHelper {
         return lottoResult;
     }
 
-    // 맞은 로또 결과 저장할 Map 초기화
     private static Map<Rank, Integer> createResult(){
         Map<Rank, Integer> lottoResult = new HashMap<>();
 
@@ -85,12 +78,10 @@ public class GameHelper {
         return (double)totalGetMoney / lottoPrice;
     }
 
-    // 구한 당첨결과 맵을 이용해서 총 상금 구하는 메서드
     private static int getLottoTotalMoney(Map<Rank, Integer> lottoWinResult) {
         int totalWinMoney = 0;
 
         for(Rank rank : lottoWinResult.keySet()) {
-            // 따낸 로또 랭크가 있다면 totalWinMoney에 더하기
             if(lottoWinResult.get(rank) > 0) {
                 totalWinMoney += rank.getWinningMoney();
             }
