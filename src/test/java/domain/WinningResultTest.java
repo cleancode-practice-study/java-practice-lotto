eae51a5809458bbb6765413cd9359dc5b6dfce3d
Egenieee <hhgg0925@naver.com>
2022-03-08 07:18:47 +0900

    -refactor: WinningResultTest 테스트 추가

--- src/test/java/domain/WinningResultTest.java
+++ src/test/java/domain/WinningResultTest.java
@@ -32,7 +32,34 @@ public class WinningResultTest {
         // create lottoWinResult
         WinningResult lottoWinResult = lottoes.getLottoWinResult(winningLotto);
 
-        assertThat(lottoWinResult.getLottoResult().get(Rank.FIRST)).isEqualTo(1);
-        assertThat(lottoWinResult.getLottoResult().get(Rank.SECOND)).isEqualTo(1);
+        assertThat(lottoWinResult.getCountOfLottoResult(Rank.FIRST)).isEqualTo(1);
+        assertThat(lottoWinResult.getCountOfLottoResult(Rank.SECOND)).isEqualTo(1);
+    }
+
+    @Test
+    public void 총_당첨금_구하기() {
+        // create lotto
+        List<Integer> lottoOne = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> lottoTwo = Arrays.asList(1, 2, 3, 4, 5, 7);
+
+        Lotto firstLotto = new Lotto(lottoOne);
+        Lotto secondLotto = new Lotto(lottoTwo);
+
+        List<Lotto> lottos = Arrays.asList(firstLotto, secondLotto);
+
+        Lottoes lottoes = new Lottoes(lottos);
+
+        // create winningLotto
+        List<Integer> winningLottoNumber = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonus = 7;
+
+        Lotto winLotto = new Lotto(winningLottoNumber);
+
+        WinningLotto winningLotto = new WinningLotto(winLotto, bonus);
+
+        // create lottoWinResult
+        WinningResult lottoWinResult = lottoes.getLottoWinResult(winningLotto);
+
+        assertThat(lottoWinResult.getLottoTotalMoney()).isEqualTo(2030000000);
     }
 }
